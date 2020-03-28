1202becdf6eceaad4a3419dfe0a567dcb26f48a9
skvisual <southkvisual@gmail.com>
2020-03-27 19:04:00 -0500

    initial push

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,29 @@
+// TODO: Write code to define and export the Employee class
+
+class Employee {
+
+  constructor(name, id, email){
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  };
+
+  getName(){
+      inquirer
+      .prompt([
+        {
+          type: 'input',
+          message: 'Enter Employee Name:',
+          name: 'empName'
+        }
+      ]).then(function({empName}){}
+    };
+  };
+
+  getId()
+
+  getEmail()
+
+  getRole()
+
+    
